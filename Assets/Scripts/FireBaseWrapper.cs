abc0a49c6d0f0c6602a3316526a54983b1c2ff09
vendigomania <vendigomania@gmail.com>
2024-01-04 23:16:52 +0300

    Add Firebase notifications

--- Assets/Scripts/FireBaseWrapper.cs
+++ Assets/Scripts/FireBaseWrapper.cs
@@ -23,7 +23,12 @@ public class FireBaseWrapper : MonoBehaviour
             }
             _callback?.Invoke(dependencyStatus == Firebase.DependencyStatus.Available);
         });
+
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
     }
+
+    #region remote config
 
     void InitializeFirebase()
     {
@@ -82,4 +87,21 @@ public class FireBaseWrapper : MonoBehaviour
                 break;
         }
     }
+
+    #endregion
+
+
+    #region notifications
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+
+    #endregion
 }
